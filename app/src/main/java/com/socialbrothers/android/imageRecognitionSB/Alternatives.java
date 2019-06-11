b10e6ae94330fc300e06c68b9b25f748510a6810
Ralph de Horde <ralph.de.horde@hva.nl>
2019-06-11 10:57:50 +0200

    Àdded strings to the new product manager

--- app/src/main/java/com/socialbrothers/android/imageRecognitionSB/Alternatives.java
+++ app/src/main/java/com/socialbrothers/android/imageRecognitionSB/Alternatives.java
@@ -13,85 +13,85 @@ import com.socialbrothers.android.imageRecognitionSB.Otherthings.ProductManager;
 import com.socialbrothers.android.imageRecognitionSB.View.ShoppingCartActivity;
 
 public class Alternatives extends AppCompatActivity {
-
-    //variables for alternative screen
-    private TextView mProduct;
-    private Button toShoppingCart, scanAgain;
-    public static String EDIT_PRODUCT  = "EDIT_PRODUCT";
-    private Product Apple, Banana, Pear;
-    String scannedProduct;
-    private ImageView imgViewAlternative1, imgViewAlternative2, imgViewAlternative3;
-    public static final String KEY_PRODUCT = "1234";
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        setContentView(R.layout.activity_alternatives);
-        DisplayMetrics dm = new DisplayMetrics();
-        getWindowManager().getDefaultDisplay().getMetrics(dm);
-        int width = dm.widthPixels;
-        int height = dm.heightPixels;
-        getWindow().setLayout((int)(width*.9),(int)(height*.7));
-
-        /*
-        * Sets up the alternative screen views.
-        * Also gets the right product from ProductManager to display the right picture.
-        * */
-        imgViewAlternative1 = findViewById(R.id.imgViewAlternative1);
-        imgViewAlternative2 = findViewById(R.id.imgViewAlternative2);
-        imgViewAlternative3 = findViewById(R.id.imgViewAlternative3);
-
-        Apple = ProductManager.getProduct("apple");
-        imgViewAlternative1.setImageResource(Apple.getResourceId());
-
-        Banana = ProductManager.getProduct("banana");
-        imgViewAlternative2.setImageResource(Banana.getResourceId());
-
-        Pear = ProductManager.getProduct("pear");
-        imgViewAlternative3.setImageResource(Pear.getResourceId());
-
-        mProduct = findViewById(R.id.productName);
-        toShoppingCart = findViewById(R.id.toShoppingCart);
-        scanAgain = findViewById(R.id.scanAgain);
-        Intent intent = getIntent();
-        scannedProduct = intent.getStringExtra(EDIT_PRODUCT);
-        mProduct.setText(scannedProduct);
-        buttonActivity();
-    }
-
-    /*
-    * ButtonActivity function handles all the button clicks done on the alternative screen.
-    * Sends the right intents through, and makes sure that the right activity is called.
-    * */
-    private void buttonActivity(){
-        toShoppingCart.setOnClickListener(v -> {
-            Intent intent = new Intent(this, ShoppingCartActivity.class);
-            Product product = ProductManager.getProduct(scannedProduct);
-            intent.putExtra(KEY_PRODUCT, product);
-            this.startActivity(intent);
-        });
-        scanAgain.setOnClickListener(v -> {
-            Intent intent = new Intent(this, CameraActivity.class);
-            this.startActivity(intent);
-        });
-
-        imgViewAlternative1.setOnClickListener(v -> {
-            Intent intent = new Intent(this, ShoppingCartActivity.class);
-            intent.putExtra(KEY_PRODUCT, Apple);
-            this.startActivity(intent);
-        });
-        imgViewAlternative2.setOnClickListener(v -> {
-            Intent intent = new Intent(this, ShoppingCartActivity.class);
-            intent.putExtra(KEY_PRODUCT, Banana);
-            this.startActivity(intent);
-        });
-        imgViewAlternative3.setOnClickListener(v -> {
-            Intent intent = new Intent(this, ShoppingCartActivity.class);
-            intent.putExtra(KEY_PRODUCT, Pear);
-            this.startActivity(intent);
-        });
-    }
-
-
+	
+	//variables for alternative screen
+	private TextView mProduct;
+	private Button toShoppingCart, scanAgain;
+	public static String EDIT_PRODUCT = "EDIT_PRODUCT";
+	private Product Apple, Banana, Pear;
+	String scannedProduct;
+	private ImageView imgViewAlternative1, imgViewAlternative2, imgViewAlternative3;
+	public static final String KEY_PRODUCT = "1234";
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.activity_alternatives);
+		DisplayMetrics dm = new DisplayMetrics();
+		getWindowManager().getDefaultDisplay().getMetrics(dm);
+		int width = dm.widthPixels;
+		int height = dm.heightPixels;
+		getWindow().setLayout((int) (width * .9), (int) (height * .7));
+		
+		/*
+		 * Sets up the alternative screen views.
+		 * Also gets the right product from ProductManager to display the right picture.
+		 * */
+		imgViewAlternative1 = findViewById(R.id.imgViewAlternative1);
+		imgViewAlternative2 = findViewById(R.id.imgViewAlternative2);
+		imgViewAlternative3 = findViewById(R.id.imgViewAlternative3);
+		
+		Apple = ProductManager.getProduct("Appel");
+		imgViewAlternative1.setImageResource(Apple.getResourceId());
+		
+		Banana = ProductManager.getProduct("Banaan");
+		imgViewAlternative2.setImageResource(Banana.getResourceId());
+		
+		Pear = ProductManager.getProduct("Peer");
+		imgViewAlternative3.setImageResource(Pear.getResourceId());
+		
+		mProduct = findViewById(R.id.productName);
+		toShoppingCart = findViewById(R.id.toShoppingCart);
+		scanAgain = findViewById(R.id.scanAgain);
+		Intent intent = getIntent();
+		scannedProduct = intent.getStringExtra(EDIT_PRODUCT);
+		mProduct.setText(scannedProduct);
+		buttonActivity();
+	}
+	
+	/*
+	 * ButtonActivity function handles all the button clicks done on the alternative screen.
+	 * Sends the right intents through, and makes sure that the right activity is called.
+	 * */
+	private void buttonActivity() {
+		toShoppingCart.setOnClickListener(v -> {
+			Intent intent = new Intent(this, ShoppingCartActivity.class);
+			Product product = ProductManager.getProduct(scannedProduct);
+			intent.putExtra(KEY_PRODUCT, product);
+			this.startActivity(intent);
+		});
+		scanAgain.setOnClickListener(v -> {
+			Intent intent = new Intent(this, CameraActivity.class);
+			this.startActivity(intent);
+		});
+		
+		imgViewAlternative1.setOnClickListener(v -> {
+			Intent intent = new Intent(this, ShoppingCartActivity.class);
+			intent.putExtra(KEY_PRODUCT, Apple);
+			this.startActivity(intent);
+		});
+		imgViewAlternative2.setOnClickListener(v -> {
+			Intent intent = new Intent(this, ShoppingCartActivity.class);
+			intent.putExtra(KEY_PRODUCT, Banana);
+			this.startActivity(intent);
+		});
+		imgViewAlternative3.setOnClickListener(v -> {
+			Intent intent = new Intent(this, ShoppingCartActivity.class);
+			intent.putExtra(KEY_PRODUCT, Pear);
+			this.startActivity(intent);
+		});
+	}
+	
+	
 }
--- app/src/main/java/com/socialbrothers/android/imageRecognitionSB/Otherthings/ProductManager.java
+++ app/src/main/java/com/socialbrothers/android/imageRecognitionSB/Otherthings/ProductManager.java
@@ -18,6 +18,7 @@ public class ProductManager {
 	private static Resources resources;
 	
 	public static Product getProduct(String name) {
+		Log.d("Name", name);
 		if (products.containsKey(name.trim().toLowerCase())) {
 			return products.get(name.trim().toLowerCase());
 		} else
@@ -29,15 +30,15 @@ public class ProductManager {
 	 * Also sets the right image, sets the right description string and the price so they are
 	 * */
 	private static void addProducts() {
-		products.put("apple", new Product("Appel", 0.6, R.drawable.apple, resources.getString(R.string.appelDesc)));
-		products.put("banana", new Product("Banaan", 0.4, R.drawable.banana, resources.getString(R.string.banaanDesc)));
+		products.put("appel", new Product("Appel", 0.6, R.drawable.apple, resources.getString(R.string.appelDesc)));
+		products.put("banaan", new Product("Banaan", 0.4, R.drawable.banana, resources.getString(R.string.banaanDesc)));
 		products.put("croissant", new Product("Croissant", 5.6, R.drawable.croissant, resources.getString(R.string.croissantDesc)));
-		products.put("cucumber", new Product("Komkommer", 5.6, R.drawable.cucumber, resources.getString(R.string.komkommerDesc)));
-		products.put("lemon", new Product("Citroen", 5.6, R.drawable.lemon, resources.getString(R.string.citroenDesc)));
-		products.put("orange", new Product("Sinaasappel", 5.6, R.drawable.orange, resources.getString(R.string.sinaasappelDesc)));
-		products.put("pear", new Product("Peer", 0.5, R.drawable.pear, resources.getString(R.string.peerDesc)));
-		products.put("tomato", new Product("Tomaat", 5.6, R.drawable.tomato, resources.getString(R.string.tomaatDesc)));
-		products.put("zucchini", new Product("Courgette", 5.6, R.drawable.zucchini, resources.getString(R.string.courgetteDesc)));
+		products.put("komkommer", new Product("Komkommer", 5.6, R.drawable.cucumber, resources.getString(R.string.komkommerDesc)));
+		products.put("citroen", new Product("Citroen", 5.6, R.drawable.lemon, resources.getString(R.string.citroenDesc)));
+		products.put("sinaasappel", new Product("Sinaasappel", 5.6, R.drawable.orange, resources.getString(R.string.sinaasappelDesc)));
+		products.put("peer", new Product("Peer", 0.5, R.drawable.pear, resources.getString(R.string.peerDesc)));
+		products.put("tomaat", new Product("Tomaat", 5.6, R.drawable.tomato, resources.getString(R.string.tomaatDesc)));
+		products.put("courgette", new Product("Courgette", 5.6, R.drawable.zucchini, resources.getString(R.string.courgetteDesc)));
 	}
 	
 	/*
